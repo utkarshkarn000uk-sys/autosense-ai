cc16bccb2997d41dc4f668c5b4523b3b6987e9a9
utkarshkarn000uk-sys <utkarshkarn000.uk@gmail.com>
2026-06-20 10:29:33 +0530

    Update API URL to Railway

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,15 +1,29 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://proud-stillness-production-16e0.up.railway.app',
 })
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`
+    config.headers['Authorization'] = `Bearer ${token}`
   }
   return config
+}, (error) => {
+  return Promise.reject(error)
 })
+
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token')
+      localStorage.removeItem('username')
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default API
